0cd46abb93f4909e23eba59e8041cce683c27b68
Crespo Wang <crespo.wang@servian.com.au>
2016-08-01 22:47:32 +1000

    devloping

--- angular-dazhaohu.js
+++ angular-dazhaohu.js
@@ -5,8 +5,33 @@
     return angular.module('angular-dazhaohu', [])
     .filter('dazhaohu', [function(){
       return function(value){
+
         console.log(value);
-        return 'hello';
+        if (value != undefined && value != null &&  typeof value.getHours === 'function')
+        {
+            if (value.getHours() >= 0 && value.getHours() < 3){
+              return "半夜三更好";
+            }
+            if (value.getHours() >= 3 && value.getHours() < 6){
+              return "清晨好";
+            }
+            if (value.getHours() >= 6 && value.getHours() < 9){
+              return "早上好";
+            }
+            if (value.getHours() >= 9 && value.getHours() < 12){
+              return "上午好";
+            }
+            if (value.getHours() >= 12 && value.getHours() < 14){
+              return "中午好";
+            }
+            if (value.getHours() >= 14 && value.getHours() < 18){
+              return "下午好";
+            }
+            if (value.getHours() >= 18 && value.getHours() < 24){
+              return "晚上好";
+            }
+        }
+        return '';
       }
     }]);
   }
