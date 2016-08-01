871a7f13ee88a2302f44c47cd26ebb6dcf367a8a
Crespo Wang <crespo.wang@servian.com.au>
2016-08-01 22:20:28 +1000

    init commit

---
+++ angular-dazhaohu.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  return angular.module('angular-dazhaohu', [])
+  .fitler('dazhaohu', [function(){
+    return function(value){
+      return 'hello';
+    }
+  }]);
+})():
